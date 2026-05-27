b7bc7129260b3fc9f295049e8349830cdd40c3c8
OlivaresY <124736762+OlivaresY@users.noreply.github.com>
2026-05-27 11:15:37 -0600

    chore: agrega Lombok al modelo Producto creando la plantilla

--- src/main/java/com/panaderia_tu_pan/sistema_pedidos/model/Producto.java
+++ src/main/java/com/panaderia_tu_pan/sistema_pedidos/model/Producto.java
@@ -1,4 +1,16 @@
 package com.panaderia_tu_pan.sistema_pedidos.model;
+import lombok.*;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class Producto {
+    private Long id;
+    private String nombre;
+    private String descripcion;
+    private double precio;
+    private boolean disponible;
 }
+
