993a2ea4e51c76aaa715a68e46b6723629761f66
OlivaresY <124736762+OlivaresY@users.noreply.github.com>
2026-05-26 19:43:38 -0600

    Creando las carpetas principales donde trabajaremos y el esqueleto de Spring Boot (MVC)

---
+++ src/main/java/com/panaderia_tu_pan/sistema_pedidos/model/Producto.java
@@ -0,0 +1,4 @@
+package com.panaderia_tu_pan.sistema_pedidos.model;
+
+public class Producto {
+}
---
+++ src/main/java/com/panaderia_tu_pan/sistema_pedidos/repository/ProductoRepository.java
@@ -0,0 +1,4 @@
+package com.panaderia_tu_pan.sistema_pedidos.repository;
+
+public class ProductoRepository {
+}
