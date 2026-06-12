30c2c79776182a92bedbc690f2d30dfb7c7d908e
Emanuel Correa <emac6940@gmail.com>
2026-06-12 15:53:09 -0600

    Aplicacion de relgas de negocio en pedido, asiganar al pedido que se crea

---
+++ src/main/java/com/panaderia_tu_pan/sistema_pedidos/service/PedidoService.java
@@ -0,0 +1,35 @@
+package com.panaderia_tu_pan.sistema_pedidos.service;
+
+import com.panaderia_tu_pan.sistema_pedidos.model.Pedido;
+import com.panaderia_tu_pan.sistema_pedidos.repository.PedidoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PedidoService {
+
+    private final PedidoRepository pedidoRepository;
+
+    public PedidoService(PedidoRepository pedidoRepository) {
+        this.pedidoRepository = pedidoRepository;
+    }
+
+    public List<Pedido> listarPedidos() {
+        return pedidoRepository.obtenerTodos();
+    }
+
+    // Crear pedido
+    public void crearPedido(Pedido pedido) {
+
+        pedido.setTotal(
+                pedido.getProducto().getPrecio()
+        );
+
+        pedido.setEstado("NUEVO");
+
+        pedidoRepository.guardar(pedido);
+    }
+
+
+}
