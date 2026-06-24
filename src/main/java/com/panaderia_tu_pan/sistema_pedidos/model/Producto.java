0fb61abcf19f5d4f689130f78b5039cf0f20facc
OlivaresY <yordy.olivaresrosales@gmail.com>
2026-06-23 20:57:14 -0600

    feat: configurar dependencias JPA y convertir Producto en entidad

--- src/main/java/com/panaderia_tu_pan/sistema_pedidos/model/Producto.java
+++ src/main/java/com/panaderia_tu_pan/sistema_pedidos/model/Producto.java
@@ -1,12 +1,18 @@
 package com.panaderia_tu_pan.sistema_pedidos.model;
-import lombok.*;
 
+import lombok.*;
+import jakarta.persistence.*;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity // indica que esto es una tabla de BD
+@Table(name = "productos") // nombre de la tabla
 
-public class Producto {
+public class Producto{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto incremental
     private Long id;
     private String nombre;
     private String descripcion;
