580208dca4a4c7cd5a8fc3ae69f50ac9516a5b10
OlivaresY <yordy.olivaresrosales@gmail.com>
2026-06-24 23:29:01 -0600

    feat: implementar entidad Pedido con relación ManyToOne a Producto

--- src/main/java/com/panaderia_tu_pan/sistema_pedidos/model/Pedido.java
+++ src/main/java/com/panaderia_tu_pan/sistema_pedidos/model/Pedido.java
@@ -1,5 +1,6 @@
 package com.panaderia_tu_pan.sistema_pedidos.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,11 +8,20 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "pedidos") //crea la tabla en MySQL
 public class Pedido {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //MySQL genere el ID
     private Long id;
+
     private String nombreCliente;
+
+    @ManyToOne //muchos pedidos pueden tener  muchos productos
+    @JoinColumn(name = "producto_id") //FK en tabla pedidos
     private Producto producto;
+
     private double total;
     private String estado;
 }
