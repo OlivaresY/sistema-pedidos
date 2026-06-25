75500a33c8aac7940c1fe14f424204bac40b1218
OlivaresY <yordy.olivaresrosales@gmail.com>
2026-06-24 23:32:55 -0600

    feat: implementar persistencia de pedidos con Spring Data JPA

--- src/main/java/com/panaderia_tu_pan/sistema_pedidos/repository/PedidoRepository.java
+++ src/main/java/com/panaderia_tu_pan/sistema_pedidos/repository/PedidoRepository.java
@@ -1,37 +1,9 @@
 package com.panaderia_tu_pan.sistema_pedidos.repository;
 
 import com.panaderia_tu_pan.sistema_pedidos.model.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Repository
-public class PedidoRepository {
-
-    // Lista donde se almacenarán los pedidos
-    private final List<Pedido> listaPedidos = new ArrayList<>();
-
-    //Simula un ID autoincremental
-    private Long idContador = 1L;
-
-    //Obtener todos los pedidos
-    public List<Pedido> obtenerTodos() {
-        return listaPedidos;
-    }
-
-    //Guardar un pedido
-    public void guardar(Pedido pedido) {
-        pedido.setId(idContador);
-        listaPedidos.add(pedido);
-        idContador++;
-    }
-
-    //Buscar pedido por ID
-    public Pedido buscarPorId(Long id) {
-        return listaPedidos.stream()
-                .filter(p -> p.getId().equals(id))
-                .findFirst()
-                .orElse(null);
-    }
+public interface PedidoRepository extends JpaRepository<Pedido, Long> { //JpaRepository  trae .findAll(), .save(), .findById()
 }
